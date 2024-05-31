47d163e8c44a45cff25a03627a16103bedd80675
Favour Chidera Emeka <favorodera@gmail.com>
2024-05-31 14:15:39 +0100

    update

--- assets/vue/index.vue
+++ assets/vue/index.vue
@@ -1,7 +1,10 @@
-<template>
+<script setup>
+import FetchInfo from './components/FetchInfo.vue'
+</script>
 
+<template>
   <main class="main-container">
     hello
+    <FetchInfo />
   </main>
-
 </template>
