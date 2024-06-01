acb5314c8101aa157e8296ae55c689952caf1b0b
Favour Chidera Emeka <favorodera@gmail.com>
2024-06-01 15:13:48 +0100

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ import '../css/index.css'
 import '../css/left-tabs.css'
 import '../css/right-tabs.css'
 import '../css/skeleton.css'
+import '../css/media-query.css'
 
 const appRootContainer = document.querySelector(".app-root-container")
 
